8f373288c243051f48351a46473f7a21c5631e98
wfj <wufangjie1223@126.com>
2017-08-14 15:00:13 +0800

    复习和新的学习

--- pyspark_test.py
+++ pyspark_test.py
@@ -88,11 +88,13 @@ print(x.combineByKey(createCombiner, mergeValue, mergeCombiner).collect())
 
 ################################################################################
 # page rank
+import numpy as np
 n = 10
 a = np.random.randint(0, 2, (n, n))
 
 pageLink = sc.parallelize([(i, j) for i in range(n) for j in range(n)
                            if i != j and a[i, j]]).groupByKey().persist()
+print(pageLink.mapValues(list).collect())
 pageRank = pageLink.mapValues(lambda x: 1.0)
 
 
@@ -107,9 +109,9 @@ for i in range(10):
         lambda x: 0.15 + 0.85 * x)
 
 
-def f(splitIndex, iterator):
-    for v in iterator:
-        yield splitIndex, v
+# def f(splitIndex, iterator):
+#     for v in iterator:
+#         yield splitIndex, v
 ################################################################################
 
 
@@ -199,3 +201,164 @@ tic = time.time()
 for i in range(40):
     scores = optimize(data)
     print('epoch: {}, cost: {}s, best_score: {}'.format(i, time.time() - tic, scores[0]))
+
+
+
+########################################################################
+# 温故
+########################################################################
+rdd = sc.parallelize(range(10))
+rdd2 = sc.parallelize([('a', 1), ('b', 2), ('a', 3), ('b', 4), ('a', 5)])
+
+print(rdd2.mapValues(lambda x: (1, x)).union(rdd2.mapValues(lambda x: (2, x))).groupByKey().mapValues(list).collect())
+print(rdd2.cogroup(rdd2).mapValues(lambda x: [list(y) for y in x]).collect())
+
+rdd3 = sc.parallelize([('a', 1)])
+print(rdd2.subtractByKey(rdd3).collect())
+def filter_func(pair):
+    key, (val1, val2) = pair
+    return val1 and not val2
+print(rdd2.cogroup(rdd3).filter(filter_func).mapValues(lambda x: x[0]).collect())
+print(rdd2.cogroup(rdd3).filter(filter_func).flatMapValues(lambda x: x[0]).collect())
+
+def collect_partition(i):
+    def func(j, iterator):
+        if j < i:
+            # yield from iterator
+            yield list(iterator)
+    return func
+
+print(rdd.mapPartitionsWithIndex(collect_partition(2), True).collect())
+
+
+
+
+def dispatch(seq):
+    vbuf, wbuf = [], []
+    for (n, v) in seq:
+        if n == 1:
+            vbuf.append(v)
+        elif n == 2:
+            wbuf.append(v)
+    return ((v, w) for v in vbuf for w in wbuf)
+
+rdd2.mapValues(lambda x: (1, x)).union(rdd2.mapValues(lambda x: (2, x))).groupByKey().flatMapValues(dispatch).collect()
+
+
+
+tmp = sc.parallelize([('a', 1), ('b', 2), ('1', 3), ('d', 4), ('2', 5)])
+print(tmp.sortByKey().collect())
+
+
+a = sc.accumulator(1)
+#rdd.foreach(lambda x: a.add(x))
+#rdd.foreach(lambda x: a = a + x)
+#rdd.foreach(lambda x: a += x)
+print(a.value)
+
+
+
+import os
+import numpy as np
+from pyspark.mllib.classification import LogisticRegressionWithSGD
+
+
+spark_home = '/home/wfj/spark-2.0.2-bin-hadoop2.7/data/mllib/'
+
+# RDD based api 要预测的值放在第一列
+# dataframe based api
+
+########################################################################
+# Binary Classification
+########################################################################
+# Load and parse the data
+data = sc.textFile(os.path.join(spark_home, 'sample_svm_data.txt'))
+parsedData = data.map(lambda line: np.array([float(x) for x in line.split(' ')]))
+model = LogisticRegressionWithSGD.train(parsedData)
+
+# Build the model
+labelsAndPreds = parsedData.map(lambda point: (int(point.item(0)),
+        model.predict(point.take(range(1, point.size)))))
+
+# Evaluating the model on training data
+trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+print("Training Error = " + str(trainErr))
+
+########################################################################
+# Clustering
+########################################################################
+from pyspark.mllib.clustering import KMeans
+from math import sqrt
+
+# Load and parse the data
+data = sc.textFile(os.path.join(spark_home, 'kmeans_data.txt'))
+parsedData = data.map(lambda line: np.array([float(x) for x in line.split(' ')]))
+
+# Build the model (cluster the data)
+clusters = KMeans.train(parsedData, 2, maxIterations=10)
+        #runs=30 initialization_mode="random")
+
+# Evaluate clustering by computing Within Set Sum of Squared Errors
+def error(point):
+    center = clusters.centers[clusters.predict(point)]
+    return sqrt(np.sum((point - center) ** 2))
+    #return sqrt(sum([x**2 for x in (point - center)]))
+
+WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
+print("Within Set Sum of Squared Error = " + str(WSSSE))
+
+
+########################################################################
+# SparkSession
+#
+# dataframe, transformer, estimator, pipeline
+########################################################################
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# df = spark.read.json('/home/wfj/spark-2.0.2-bin-hadoop2.7/examples/src/main/resources/people.json')
+# df.createOrReplaceTempView("people")
+# sqlDF = spark.sql("SELECT * FROM people") # 直接运行 sql 语句
+# sqlDF.show()
+
+# 使用 pipeline
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import HashingTF, Tokenizer
+
+# Prepare training documents from a list of (id, text, label) tuples.
+training = spark.createDataFrame([
+    (0, "a b c d e spark", 1.0),
+    (1, "b d", 0.0),
+    (2, "spark f g h", 1.0),
+    (3, "hadoop mapreduce", 0.0)
+], ["id", "text", "label"]) # NOTE column names, 下面会用到
+
+# Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and lr.
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="features")
+lr = LogisticRegression(maxIter=10, regParam=0.001)
+pipeline = Pipeline(stages=[tokenizer, hashingTF, lr])
+
+# Fit the pipeline to training documents.
+model = pipeline.fit(training)
+
+# Prepare test documents, which are unlabeled (id, text) tuples.
+test = spark.createDataFrame([
+    (4, "spark i j k"),
+    (5, "l m n"),
+    (6, "spark hadoop spark"),
+    (7, "apache hadoop")
+], ["id", "text"])
+
+# Make predictions on test documents and print columns of interest.
+prediction = model.transform(test) # NOTE is transform, no predict
+selected = prediction.select("id", "text", "probability", "prediction")
+for row in selected.collect():
+    rid, text, prob, prediction = row
+    print("(%d, %s) --> prob=%s, prediction=%f" % (rid, text, str(prob), prediction))
